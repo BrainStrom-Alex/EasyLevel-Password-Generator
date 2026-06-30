7b17f9f91ba8d034d0f19ad035e1a6720b17b844
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-30 15:57:16 +0530

    convert pass to string

--- main.py
+++ main.py
@@ -36,6 +36,12 @@ for sym in range(0, nr_symbols):
 for num in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
 
-random.shuffle(password_list)
-
 print(password_list)
+
+random.shuffle(password_list)
+print(password_list)
+
+password  = ""
+for char in password_list:
+    password += char
+print(password)
