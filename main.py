336abebc29e53b52d42e1d549a7a6e0bec4a8646
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-30 15:37:12 +0530

    add password system

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+
+for char in range(0, nr_letters):
+    password += random.choice(letters)
+
+for sym in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+for num in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
